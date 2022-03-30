7ef10c4a6d4746d3f3630381ce9a8a6905d7f2ff
Felix Pessoa da Silv <Felix Pessoa da Silv@NB-TI002626>
2022-03-30 13:13:35 -0300

    Commit inicial do projeto

---
+++ src/test/java/com/felix/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felix.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
